5ba2f26f6efe227aac7b2beabe0effc443c84c2c
Sushrut Mujumdar <91233430+spmujumdarNEU@users.noreply.github.com>
2022-11-30 20:13:24 -0500

    health with msg

--- tests/api.test.js
+++ tests/api.test.js
@@ -4,7 +4,7 @@ const request = require("supertest");
 describe("GET /health", () => {
     test("Testing GET API", async() => {
         const response = await request(app).get("/health");
-        expect(response.body).toEqual("server responds with 200 OK if it is healht.");
+        expect(response.body).toEqual("server responds with 200 OK if it is healhty.");
         expect(response.statusCode).toBe(200);
     });
 });
