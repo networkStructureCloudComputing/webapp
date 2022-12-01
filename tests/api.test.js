9a17c769d707e2439834019f3e2c3b49a826f433
Sushrut Mujumdar <91233430+spmujumdarNEU@users.noreply.github.com>
2022-11-30 20:17:03 -0500

    Assignment 09 (#41)
    * CiCd
    
    * health
    
    * health with msg

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,9 +1,9 @@
 const app = require("../api");
 const request = require("supertest");
 
-describe("GET /healthz", () => {
+describe("GET /health", () => {
     test("Testing GET API", async() => {
-        const response = await request(app).get("/healthz");
+        const response = await request(app).get("/health");
         expect(response.body).toEqual("server responds with 200 OK if it is healhty.");
         expect(response.statusCode).toBe(200);
     });
